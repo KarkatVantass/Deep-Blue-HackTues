0fadc432f33b5b41f97925deb2cd7c30530f23f3
KarkatVantass <marty.ganev@gmail.com>
2018-03-16 01:59:26 +0200

    Added: scenes MainMenu and Intro. Unfinished main menu UI

---
+++ DeepBlue/Assets/Scripts/UI/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+    public void loadSceneByIndex(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+}
