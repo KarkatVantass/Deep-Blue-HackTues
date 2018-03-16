41aba2bc1c0d40a13a5afad309d663203a592772
AlexandarMilissov <alex.milissov@gmail.com>
2018-03-16 15:58:24 +0200

    added player movement combat and interaction

---
+++ DeepBlue/Assets/Scripts/Gameplay/Chest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour {
+
+    public GameObject character;
+    public float interactionRadius = 5;
+    MeshRenderer mr;
+
+    void Start()
+    {
+        mr = this.GetComponentInChildren<MeshRenderer>();
+    }
+
+    void Update () {
+
+        bool isInRange;
+
+        if (
+            character.transform.position.y < (this.transform.position.y + interactionRadius)
+            && character.transform.position.y > (this.transform.position.y - interactionRadius)
+            && character.transform.position.x < (this.transform.position.x + interactionRadius)
+            && character.transform.position.x > (this.transform.position.x - interactionRadius)
+            )
+        {
+            mr.material.color = Color.red;
+            isInRange = true;
+        }
+        else
+        {
+            mr.material.color = Color.white;
+            isInRange = false;
+        }
+
+        if(isInRange && Input.GetKeyDown("space"))
+        {
+            Debug.Log("i am wroking");
+        }
+	}
+}
---
+++ DeepBlue/Assets/Scripts/Gameplay/Combat.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Combat : MonoBehaviour {
+
+
+    public float hitPoints = 100;
+    public float attackRange = 3;
+    public float attackDmg = 30;
+    public float cooldown = 1;
+    float LastTime;
+
+    void Start()
+    {
+        LastTime = Time.time;
+    }
+    
+	void Update ()
+    {
+        if (hitPoints<=0)
+        {
+            Destroy(this.gameObject);
+        }
+		if(Input.GetKeyDown("e") && (LastTime + cooldown <= Time.time))
+        {
+            Attack();
+        }
+        else
+        {
+            Debug.Log(Time.time);
+            Debug.Log(LastTime);
+        }
+	}
+    void Attack()
+    {
+        LastTime = Time.time;
+        Debug.Log("attack");
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(this.transform.position, attackRange);
+        foreach(var enemy in enemies.Where(x => x.gameObject.tag == "Enemy"))
+        {
+            GameObject target = enemy.gameObject;
+
+            target.GetComponent<Enemy>().TakeDmg(attackDmg);
+        }
+    }
+}
