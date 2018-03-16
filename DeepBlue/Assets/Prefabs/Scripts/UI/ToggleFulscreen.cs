9285fd15f3540451441f7c81f7ccfd49268b395d
KarkatVantass <marty.ganev@gmail.com>
2018-03-16 21:54:09 +0200

    Main Menu UI ready(without scripts). Added buttons to levelUI

---
+++ DeepBlue/Assets/Prefabs/Scripts/UI/ToggleFulscreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleFulscreen : MonoBehaviour {
+    public GameObject toggle;
+
+    public void toggleFulscreen() {
+        Screen.fullScreen = !Screen.fullScreen;
+        
+    }
+}
