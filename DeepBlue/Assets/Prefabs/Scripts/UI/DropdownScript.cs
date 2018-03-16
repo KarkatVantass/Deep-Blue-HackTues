852a62e64c925236272cf28178c49ab42405f8f9
KarkatVantass <marty.ganev@gmail.com>
2018-03-16 19:27:16 +0200

    added graphics UI

---
+++ DeepBlue/Assets/Prefabs/Scripts/UI/DropdownScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownScript : MonoBehaviour {
+    private Resolution[] resolutions;
+    public Dropdown resMenu; 
+	// Use this for initialization
+	void Start () {
+        this.resolutions = Screen.resolutions;
+        for (int i = 0; i < resolutions.Length - 1; i++)
+        {
+            resMenu.options.Add(new Dropdown.OptionData(resToText(resolutions[i])));
+
+            resMenu.value = i;
+
+            resMenu.onValueChanged.AddListener(delegate { Screen.SetResolution(resolutions[resMenu.value].width, resolutions[resMenu.value].height, Screen.fullScreen);  } );
+        }
+    }
+    
+
+    private string resToText(Resolution res)
+    {
+        return res.width + "x" + res.height;
+    }
+    public void onValueChanged() {
+        Screen.SetResolution(resolutions[resMenu.value].width, resolutions[resMenu.value].height, Screen.fullScreen);
+    }
+}
---
+++ DeepBlue/Assets/Prefabs/Scripts/UI/Pause.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public GameObject pauseScreen;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Escape)) {
+            this.pauseScreen.SetActive(!this.pauseScreen.activeSelf);
+        }
+	}
+}
