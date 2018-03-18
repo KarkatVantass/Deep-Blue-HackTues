0f53179879fd05acb321dc23e7cfa3acfcf9eef3
KarkatVantass <marty.ganev@gmail.com>
2018-03-18 05:14:22 +0200

    fixing git. Again

--- DeepBlue/Assets/Prefabs/Gameplay/main gameplay scripts/MovementScript.cs
+++ DeepBlue/Assets/Prefabs/Gameplay/main gameplay scripts/MovementScript.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+
 
 public class MovementScript : MonoBehaviour {
 
     Rigidbody2D rb;
+    public SpriteRenderer sprite;
 
     public float speed = 10;
     public float totalSpeed;
@@ -15,11 +18,23 @@ public class MovementScript : MonoBehaviour {
     {
         totalSpeed = speed;
         rb = this.GetComponent<Rigidbody2D>();
+        sprite = this.GetComponentInChildren<SpriteRenderer>();
     }
 
     void FixedUpdate()
     {
         var horizontalMovement = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * totalSpeed;
+
+        if (Input.GetKeyDown(KeyCode.A) && !sprite.flipX)
+        {
+            sprite.flipX = true;
+            Debug.Log("turn left");
+        }else if(Input.GetKeyDown(KeyCode.D) && sprite.flipX)
+        {
+
+            Debug.Log("turn right");
+            sprite.flipX = false;
+        }
 
         if((Input.GetAxis("Vertical") != 0)
             && isGrounded == true)
